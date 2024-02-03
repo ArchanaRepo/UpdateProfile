43ca6b1cc008def9bc3ca9f47f0a0c0044895e6e
ArchanaRepo <archana.gupta0414@gmail.com>
2024-02-03 15:24:42 +0530

    Add project files.

---
+++ NaukriUpdate/Pages/NaukriLoginPage.cs
@@ -0,0 +1,139 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Support;
+
+using SeleniumExtras.PageObjects;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace NaukriUpdate.Pages
+{
+    [Binding]
+    public class NaukriLoginPage : BasePage
+    {
+        public IWebDriver driver;
+
+        By UserName = By.Id("usernameField");
+        By Password = By.Id("passwordField");
+        By LoginButton = By.XPath("//button[text()='Login']");
+        By ProfileLink = By.XPath("//div[@class='view-profile-wrapper']//a[@href='/mnjuser/profile']");
+        By ResumeHeadlineEditIcon = By.XPath("//div[@class='resumeHeadline']//span[@class='edit icon']");
+        By ResumeHeadLineTextArea = By.XPath("//textArea[@id='resumeHeadlineTxt']");
+        By ResumeHeadLineSaveButto = By.XPath("//form[@name='resumeHeadlineForm']//button[text()='Save']");
+        By ResumeHeadline = By.XPath("//div[@class='resumeHeadline']//div[@class='prefill typ-14Medium']/div");
+        By BasicDetailsEditIcon = By.XPath("//span[@class='fullname']/..//em[text()='editOneTheme']");
+        By NameTextBox = By.XPath("//input[@id='name']");
+        By SaveBasicDetailsButtonn = By.XPath("//button[@id='saveBasicDetailsBtn']");
+        By FullNameText = By.XPath("//span[@class='fullname']");
+
+
+        public void LaunchURL()
+        {
+            driver=new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.naukri.com/mnjuser/homepage");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(5);
+            Thread.Sleep(1000);
+
+
+        }
+        public void LoginInPortal(string username,string password)
+        {
+            driver.FindElement(UserName).SendKeys(username);
+            driver.FindElement(Password).SendKeys(password);
+            driver.FindElement(LoginButton).Click();    
+            Thread.Sleep(1000);
+        }
+
+        public  void ClickOnProfile()
+        {
+            driver.FindElement(ProfileLink).Click();
+            Thread.Sleep(1000);
+        }
+
+        public void ClickOnResumeHeadline()
+        {
+            driver.FindElement(ResumeHeadlineEditIcon).Click();
+            Thread.Sleep(1000);
+        }
+        public void AddTextInResumeHeadlineTextArea(string text)
+        {
+            driver.FindElement(ResumeHeadLineTextArea).Clear();
+            driver.FindElement(ResumeHeadLineTextArea).SendKeys(text);
+            Thread.Sleep(2000);
+        }
+
+        public void CLickOnResumeHeadlineSaveButton()
+        {
+            driver.FindElement(ResumeHeadLineSaveButto).Click();
+            Thread.Sleep(1000);
+
+        }
+
+        public bool CompareResumeHeadline(string expectedText)
+        {
+            Thread.Sleep(1000);
+            Actions actions = new Actions(driver);
+            IWebElement ele = driver.FindElement(ResumeHeadline);
+            actions.MoveToElement(ele).Build().Perform();
+            var headlineFromUI = driver.FindElement(ResumeHeadline).Text;
+            if(expectedText.Equals(headlineFromUI))
+                return true;
+            else return false;
+            
+        }
+
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+        public void clickEditIconOnBasicDetails()
+        {
+            Thread.Sleep(1000);
+            driver.FindElement(BasicDetailsEditIcon).Click();
+            Thread.Sleep(5);
+        }
+
+        public void updateFullName(string fullName)
+        {
+            Thread.Sleep(5000);
+            driver.FindElement(NameTextBox).Click();
+             driver.FindElement(NameTextBox).Clear();
+            Thread.Sleep(2000);
+            driver.FindElement(NameTextBox).SendKeys(fullName);
+            Thread.Sleep(5000);
+
+        }
+
+        public void ClickOnSaveBasicDetailsButton()
+        {
+            Thread.Sleep(5000);
+            driver.FindElement(SaveBasicDetailsButtonn).Click();
+            Thread.Sleep(5000);
+        }
+
+        public bool CompareUpdatedName(string expectedText)
+        {
+            Thread.Sleep(5000);
+            Actions actions = new Actions(driver);
+            IWebElement ele = driver.FindElement(FullNameText);
+            actions.MoveToElement(ele).Build().Perform();
+            var fullNameFromUI = driver.FindElement(FullNameText).Text;
+            if (expectedText.Equals(fullNameFromUI))
+                return true;
+            else return false;
+
+        }
+       
+
+    }
+}
---
+++ NaukriUpdate/Steps/NaukriLoginSteps.cs
@@ -0,0 +1,98 @@
+﻿using NaukriUpdate.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace NaukriUpdate.Steps
+{
+    [Binding]
+    public class NaukriLoginSteps
+    {
+        NaukriLoginPage loginpage = new NaukriLoginPage();
+
+        [Given(@"I launch Naukri portal")]
+        public void LaunchNaukriPortal()
+        {
+            loginpage.LaunchURL();
+        }
+
+        [Given(@"I login using (.*) and (.*)")]
+        public void LoginInPortal(string username, string password)
+        {
+            loginpage.LoginInPortal(username, password);
+        }
+
+        [When(@"I click on profile link")]
+        public void WhenIClickOnProfileLink()
+        {
+            loginpage.ClickOnProfile();
+        }
+
+        [When(@"I click on Resume Headline Edit Icon")]
+        public void WhenIClickOnResumeHeadlineEditIcon()
+        {
+            loginpage.ClickOnResumeHeadline();
+
+        }
+
+        [When(@"I enter ResumeHeadline as (.*)")]
+        public void IEnterResumeHeadline(string text)
+        {
+            loginpage.AddTextInResumeHeadlineTextArea(text);
+        }
+
+        [When(@"I click on Resume Headline Save Button")]
+        public void WhenIClickOnResumeHeadlineSaveButton()
+        {
+            loginpage.CLickOnResumeHeadlineSaveButton();
+        }
+
+        [Then(@"I Verify Resume Headline Updated Successfully with headline as (.*)")]
+        public void ThenIVerifyResumeHeadlineUpdatedSuccessfully(string Headline)
+        {
+            Assert.That(loginpage.CompareResumeHeadline(Headline));
+        }
+
+        [Then(@"I close the browser")]
+        public void ThenICloseTheBrowser()
+        {
+            loginpage.CloseBrowser();
+        }
+        [When(@"I click on Basic Detals Edit Icon")]
+        public void WhenIClickOnBasicDetalsEditIcon()
+        {
+            loginpage.clickEditIconOnBasicDetails();
+        }
+        [When(@"I update FullName as (.*)")]
+        public void WhenIUpdateFullNameAsArchanaBShortNoticePeriod(string name)
+        {
+            loginpage.updateFullName(name);
+        }
+        [When(@"I click on save Basic Details Button")]
+        public void WhenIClickOnSaveBasicDetailsButton()
+        {
+            loginpage.ClickOnSaveBasicDetailsButton();
+        }
+        [Then(@"I should see updated name in Full name as (.*)")]
+        public void ThenIShouldSeeUpdatedNameInFullName(string updatedName)
+        {
+            Assert.That(loginpage.CompareUpdatedName(updatedName));
+        }
+
+
+    
+
+
+
+
+
+
+
+
+
+    }
+}
